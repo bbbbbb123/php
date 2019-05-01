bb9e8f59722f1dc39b597ce4e3ab369b16c23fec
bbbbbb123 <p32295@gmail.com>
2019-05-01 17:05:53 +0800

    php

---
+++ welcome.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+Welcome PHP!!!	
+Name:<?php echo $_GET["name"]; ?>
+gender:<?php echo $_POST["gender"]; ?>
+interest:<?php echo $_POST["interest"]; ?>
+location:<?php echo $_POST["location"]; ?>
+</body>
+</html>
